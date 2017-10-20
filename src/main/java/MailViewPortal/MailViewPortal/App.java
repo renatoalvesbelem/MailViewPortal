e5588e285249e886cc54f3d4f0274a67abbb494c
jose.renato <renatoalvesbelem@gmail.com>
2017-10-20 15:28:44 -0200

    Adicionado projeto ao repositório

---
+++ src/main/java/MailViewPortal/MailViewPortal/App.java
@@ -0,0 +1,13 @@
+package MailViewPortal.MailViewPortal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
