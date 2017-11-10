508240ac680f4d315c1918773608a3350ced2974
jose.renato <renatoalvesbelem@gmail.com>
2017-11-10 11:47:26 -0200

    Corrigido warnings apontados pela ferramenta de cógido

--- src/main/java/br/com/control/HTMLPageControl.java
+++ src/main/java/br/com/control/HTMLPageControl.java
@@ -18,88 +18,88 @@ import org.apache.commons.lang.StringEscapeUtils;
 import br.com.model.EmailModel;
 
 public class HTMLPageControl {
-	URL urlMailHTML;
-	private String iPServer;
-	private Map<Integer, EmailModel> titleBodyEmail;
-	private EmailModel emailModel;
+    private String iPServer;
+    private Map<Integer, EmailModel> titleBodyEmail;
 
-	public HTMLPageControl(String iPServer) {
-		this.iPServer = iPServer;
-	}
+    public HTMLPageControl(String iPServer) {
+        this.iPServer = iPServer;
+    }
 
-	public Map<Integer, EmailModel> readPage() {
-		int n = 1;
-		String urlString = "http://" + iPServer + "/mailserver/email/body/-";
-		int tmp = 0;
-		titleBodyEmail = new LinkedHashMap<Integer, EmailModel>();
-		String titleEmail = "";
-		StringBuilder bodyEmail;
-		int tmpMail = 1;
-		try {
-			if (n == 1 && checkStatusHTTP(new URL(urlString + n)) == false) {
-				JOptionPane.showMessageDialog(null, "Não existe nenhum email registrado em memória");
-			} else {
-				while (checkStatusHTTP(new URL(urlString + n))) {
-					urlMailHTML = new URL(urlString + n);
-					BufferedReader in = new BufferedReader(new InputStreamReader(urlMailHTML.openStream()));
-					String inputLine;
-					emailModel = new EmailModel();
-					bodyEmail = new StringBuilder();
-					while ((inputLine = in.readLine()) != null) {
-						if (inputLine.contains("<body>") || tmp == 1) {
-							if (inputLine.contains("</body>")) {
-								tmp = 0;
-							} else {
-								bodyEmail.append((inputLine));
-								tmp = 1;
-								tmpMail++;
-								if (tmpMail == 4 || tmpMail == 3) {
-									titleEmail = titleEmail + corrigeString(inputLine);
-								}
-							}
-						}
-					}
-					in.close();
-					tmpMail = 0;
-					emailModel.setTitleEmail(corrigeString(titleEmail));
-					titleEmail = "";
-					emailModel.setBodyEmail(bodyEmail);
-					titleBodyEmail.put(n++, emailModel);
-				}
-			}
+    private Map<Integer, EmailModel> readPage() {
+        URL urlMailHTML;
+        EmailModel emailModel;
+        int n = 1;
+        String urlString = "http://" + iPServer + "/mailserver/email/body/-";
+        int tmp = 0;
+        titleBodyEmail = new LinkedHashMap<Integer, EmailModel>();
+        StringBuilder titleEmail = new StringBuilder();
+        StringBuilder bodyEmail;
+        int tmpMail = 1;
+        try {
+            if (checkStatusHTTP(new URL(urlString + n))) {
+                while (checkStatusHTTP(new URL(urlString + n))) {
+                    urlMailHTML = new URL(urlString + n);
+                    BufferedReader in = new BufferedReader(new InputStreamReader(urlMailHTML.openStream()));
+                    String inputLine;
+                    emailModel = new EmailModel();
+                    bodyEmail = new StringBuilder();
+                    while ((inputLine = in.readLine()) != null) {
+                        if (inputLine.contains("<body>") || tmp == 1) {
+                            if (inputLine.contains("</body>")) {
+                                tmp = 0;
+                            } else {
+                                bodyEmail.append((inputLine));
+                                tmp = 1;
+                                tmpMail++;
+                                if (tmpMail == 4 || tmpMail == 3) {
+                                    titleEmail.append(corrigeString(inputLine));
+                                }
+                            }
+                        }
+                    }
+                    in.close();
+                    tmpMail = 0;
+                    emailModel.setTitleEmail(corrigeString(titleEmail.toString()));
+                    titleEmail = new StringBuilder();
+                    emailModel.setBodyEmail(bodyEmail);
+                    titleBodyEmail.put(n++, emailModel);
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Não existe nenhum email registrado em memória");
+            }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return titleBodyEmail;
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return titleBodyEmail;
+    }
 
-	private boolean checkStatusHTTP(URL URL) {
-		try {
-			return (((HttpURLConnection) URL.openConnection()).getResponseCode()) == 200;
-		} catch (ConnectException e) {
-			JOptionPane.showMessageDialog(null, "Não foi possível conectar ao servidor: " + URL.getHost());
-			return false;
-		} catch (IOException e) {
-			return false;
-		}
+    private boolean checkStatusHTTP(URL URL) {
+        try {
+            return (((HttpURLConnection) URL.openConnection()).getResponseCode()) == 200;
+        } catch (ConnectException e) {
+            JOptionPane.showMessageDialog(null, "Não foi possível conectar ao servidor: " + URL.getHost());
+            return false;
+        } catch (IOException e) {
+            return false;
+        }
 
-	}
+    }
 
-	public List<String> retornaTitulosEmails() {
-		LinkedHashMap<Integer, EmailModel> keyMap = (LinkedHashMap<Integer, EmailModel>) this.readPage();
-		List<String> keys = new ArrayList<String>();
-		for (EmailModel t : keyMap.values()) {
-			keys.add(t.getTitleEmail());
-		}
-		return keys;
-	}
+    public List<String> retornaTitulosEmails() {
+        LinkedHashMap<Integer, EmailModel> keyMap = (LinkedHashMap<Integer, EmailModel>) this.readPage();
+        List<String> keys = new ArrayList<String>();
+        for (EmailModel t : keyMap.values()) {
+            keys.add(t.getTitleEmail());
+        }
+        return keys;
+    }
 
-	private String corrigeString(String string) {
-		return (StringEscapeUtils.unescapeHtml(string)).replaceAll("<.*?>", " ");
-	}
+    private String corrigeString(String string) {
+        return (StringEscapeUtils.unescapeHtml(string)).replaceAll("<.*?>", " ");
+    }
 
-	public StringBuilder retornaBodyEmail(Integer key) {
-		return titleBodyEmail.get(key + 1).getBodyEmail();
-	}
+    public StringBuilder retornaBodyEmail(Integer key) {
+        return titleBodyEmail.get(key + 1).getBodyEmail();
+    }
 }
--- src/main/java/br/com/view/Principal.java
+++ src/main/java/br/com/view/Principal.java
@@ -6,17 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import javax.swing.JButton;
-import javax.swing.JEditorPane;
-import javax.swing.JFormattedTextField;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 import br.com.component.NonEditableModel;
@@ -24,108 +14,102 @@ import br.com.control.HTMLPageControl;
 
 public class Principal extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
-	private JScrollPane scrollPaneEmailTitle;
-	private JTable table;
-	private String[] columnNames = { "Header" };
-	private Object[][] data = {};
-	NonEditableModel model = new NonEditableModel(data, columnNames);
-	private JButton btnGo;
-	private JEditorPane editionPaneBodyEmail;
-	private HTMLPageControl htmlPageControl;
-	private JFormattedTextField txIp;
+    private static final long serialVersionUID = 1L;
+    private JTable table;
+    private String[] columnNames = {"Header"};
+    private Object[][] data = {};
+    private NonEditableModel model = new NonEditableModel(data, columnNames);
+    private JEditorPane editionPaneBodyEmail;
+    private HTMLPageControl htmlPageControl;
+    private JFormattedTextField txIp;
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Principal frame = new Principal();
-					frame.setVisible(true);
-					try {
-						UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-						SwingUtilities.updateComponentTreeUI(frame);
-					} catch (Exception e1) {
-						e1.printStackTrace();
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Principal frame = new Principal();
+                    frame.setVisible(true);
+                    try {
+                        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+                        SwingUtilities.updateComponentTreeUI(frame);
+                    } catch (Exception e1) {
+                        e1.printStackTrace();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	public Principal() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 934, 782);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+    private Principal() {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(100, 100, 934, 782);
+        JPanel contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
 
-		JLabel lblIpDoPortal = new JLabel("IP do Portal");
-		lblIpDoPortal.setBounds(10, 14, 86, 14);
-		contentPane.add(lblIpDoPortal);
+        JLabel lblIpDoPortal = new JLabel("IP do Portal");
+        lblIpDoPortal.setBounds(10, 14, 86, 14);
+        contentPane.add(lblIpDoPortal);
 
-		scrollPaneEmailTitle = new JScrollPane();
-		scrollPaneEmailTitle.setBounds(10, 68, 898, 246);
-		contentPane.add(scrollPaneEmailTitle);
+        JScrollPane scrollPaneEmailTitle = new JScrollPane();
+        scrollPaneEmailTitle.setBounds(10, 68, 898, 246);
+        contentPane.add(scrollPaneEmailTitle);
 
-		table = new JTable(data, columnNames);
-		table.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				preencheCorpoEmail(editionPaneBodyEmail, table.getSelectedRow());
-			}
-		});
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		table.setModel(model);
-		scrollPaneEmailTitle.setViewportView(table);
+        table = new JTable(data, columnNames);
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent arg0) {
+                preencheCorpoEmail(editionPaneBodyEmail, table.getSelectedRow());
+            }
+        });
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        table.setModel(model);
+        scrollPaneEmailTitle.setViewportView(table);
 
-		btnGo = new JButton("GO");
-		btnGo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				preencheTable(model, txIp.getText());
-			}
-		});
+        JButton btnGo = new JButton("GO");
+        btnGo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                preencheTable(model, txIp.getText());
+            }
+        });
 
-		btnGo.setBounds(570, 10, 89, 23);
-		contentPane.add(btnGo);
+        btnGo.setBounds(570, 10, 89, 23);
+        contentPane.add(btnGo);
 
-		editionPaneBodyEmail = new JEditorPane();
-		editionPaneBodyEmail.setEditable(false);
-		editionPaneBodyEmail.setBounds(10, 371, 898, 185);
+        editionPaneBodyEmail = new JEditorPane();
+        editionPaneBodyEmail.setEditable(false);
+        editionPaneBodyEmail.setBounds(10, 371, 898, 185);
 
-		JScrollPane scrollPaneBodyEmail = new JScrollPane(editionPaneBodyEmail);
-		scrollPaneBodyEmail.setBounds(10, 343, 898, 352);
-		contentPane.add(scrollPaneBodyEmail);
-		txIp = new JFormattedTextField();
-		txIp.setBounds(92, 11, 467, 20);
-		contentPane.add(txIp);
-	}
+        JScrollPane scrollPaneBodyEmail = new JScrollPane(editionPaneBodyEmail);
+        scrollPaneBodyEmail.setBounds(10, 343, 898, 352);
+        contentPane.add(scrollPaneBodyEmail);
+        txIp = new JFormattedTextField();
+        txIp.setBounds(92, 11, 467, 20);
+        contentPane.add(txIp);
+    }
 
-	public void preencheTable(NonEditableModel model, String iPServer) {
-		htmlPageControl = new HTMLPageControl(iPServer);
-		limpaInformacoesEmail();
-		for (String tituloEmails : htmlPageControl.retornaTitulosEmails()) {
-			model.addRow(new Object[] { tituloEmails.trim() });
-		}
-	}
+    private void preencheTable(NonEditableModel model, String iPServer) {
+        htmlPageControl = new HTMLPageControl(iPServer);
+        limpaInformacoesEmail();
+        for (String tituloEmails : htmlPageControl.retornaTitulosEmails()) {
+            model.addRow(new Object[]{tituloEmails.trim()});
+        }
+    }
 
-	public void preencheCorpoEmail(JEditorPane editionPaneBodyEmail, Integer key) {
-		editionPaneBodyEmail.setText("");
-		editionPaneBodyEmail.setContentType("text/html");
-		editionPaneBodyEmail.setText(htmlPageControl.retornaBodyEmail(key).toString());
-		editionPaneBodyEmail.setCaretPosition(0);
-	}
+    private void preencheCorpoEmail(JEditorPane editionPaneBodyEmail, Integer key) {
+        editionPaneBodyEmail.setText("");
+        editionPaneBodyEmail.setContentType("text/html");
+        editionPaneBodyEmail.setText(htmlPageControl.retornaBodyEmail(key).toString());
+        editionPaneBodyEmail.setCaretPosition(0);
+    }
 
-	private void limpaInformacoesEmail() {
-		for (int i = model.getRowCount() - 1; i >= 0; i--) {
-			model.removeRow(i);
-		}
-		editionPaneBodyEmail.setText("");
-	}
+    private void limpaInformacoesEmail() {
+        for (int i = model.getRowCount() - 1; i >= 0; i--) {
+            model.removeRow(i);
+        }
+        editionPaneBodyEmail.setText("");
+    }
 }
